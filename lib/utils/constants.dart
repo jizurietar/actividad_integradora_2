52f2e17d2775e5f2541f367bfbf3df451247a233
jizurietar <j.izurieta@eston.ecotec.edu.ec>
2026-08-27 21:31:02 -0500

    Creacion de constates para los temas y rutas

---
+++ lib/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0A2647); // azul marino
+  static const Color secondary = Color(0xFFD4AF37); // dorado
+  static const Color background = Color(0xFFF5F5F5);
+  static const Color cardBg = Colors.white;
+}
+
+class AppRoutes {
+  static const String login = '/';
+  static const String home = '/home';
+  static const String cart = '/cart';
+  static const String orders = '/orders';
+}
