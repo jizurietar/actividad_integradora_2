cbd8f1b3ebbc804a8f56b21d61de47ca822acaf4
jizurietar <j.izurieta@eston.ecotec.edu.ec>
2026-08-27 22:07:06 -0500

    shared_preferences activar las dependencias para mantener la session

--- lib/main.dart
+++ lib/main.dart
@@ -12,24 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Mi Tienda Virtual',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-        colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: AppColors.primary,
-          secondary: AppColors.secondary,
-        ),
-        scaffoldBackgroundColor: AppColors.background,
-        appBarTheme: const AppBarTheme(
-          backgroundColor: AppColors.primary,
-          foregroundColor: Colors.white,
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: AppColors.secondary,
-            foregroundColor: AppColors.primary,
-          ),
-        ),
-      ),
+
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
