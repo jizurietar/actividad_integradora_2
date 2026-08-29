77a531ec38f7d4bb64aa74082816a2ca6b3206b9
jizurietar <j.izurieta@eston.ecotec.edu.ec>
2026-08-29 11:24:56 -0500

    Afinando los iconos

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Mi Tienda Virtual',
       theme: ThemeData(
         primaryColor: AppColors.primary,
